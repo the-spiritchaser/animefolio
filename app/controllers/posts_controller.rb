5740f5d997bffc39c977227c67440a136528a322
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-03-23 12:42:28 +0900

    Fix typos

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,20 @@
 class PostsController < ApplicationController
-  
+
   def index
     @posts = Post.includes(:user).order("created_at DESC")
   end
 
+  def new
+    @post = Post.new
+  end
+
   def create
-    @post = Post.new(post_params)
+    @post = Post.create(post_params)
   end
 
   private
 
   def post_params
-    params.require(:tweet).permit(:title, :image).merge(user_id: current_user.id)
+    params.require(:post).permit(:title, :image).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:show, :edit, :update]
   resources :posts, only: [:index, :new, :create]
 end
