083ba40d60ba0f27aa50d462cd4253232573f685
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-03-25 14:43:20 +0900

    Add routes users/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'posts#index'
-  resources :users, only: [:show, :edit, :update]
+  root 'users#index'
+  resources :users, only: [:index, :show, :edit, :update]
   resources :posts, only: [:index, :show, :new, :create]
 end
