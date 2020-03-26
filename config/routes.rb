265a1c7b2dd29f2a96c659ccaefc15b82a57e092
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-03-26 14:46:27 +0900

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
   resources :users, only: [:index, :show, :edit, :update]
-  resources :posts, only: [:index, :show, :new, :create]
+  resources :posts, only: [:index, :new, :create]
 end
