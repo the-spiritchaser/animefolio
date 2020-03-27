a18dec3404037ffa8bc6df8bcd918aede4e7035a
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-03-27 14:29:58 +0900

    Add relation to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
-  
+  has_many :room_users
+  has_many :rooms, through: :room_users
+
   validates :name, presence: true, uniqueness: true
 
   include Gravtastic
