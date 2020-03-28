b148ce7735be95ff20a06672610f8d1327b37335
the-spiritchaser <the.spiritchaser4@gmail.com>
2020-03-28 22:31:54 +0900

    Add rooms_controller

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -5,14 +5,27 @@ class RoomsController < ApplicationController
     redirect_to room_path(@room)
   end
 
+  def index
+    rooms = Room.includes(:user).order("updated_at DESC")
+    @users = rooms.users
+  end
+
   def show
-    
+    @room = Room.find(params[:id])
+    room_user = @room.users
+    private_room(room_user)
   end
 
   private
-
+    
     def room_params
       params.require(:room).permit(user_ids: [])
+    end
+
+    def private_room(user)
+      unless @room.users.include?(current_user) then
+        redirect_to root_path
+      end
     end
 
 end
